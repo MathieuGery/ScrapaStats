d0e9e18db49f138a750491280276a1268423e36a
MathieuGery <mathieu.gery@epitech.eu>
2022-06-17 01:00:02 +0200

    merge datas

---
+++ main.py
@@ -0,0 +1,38 @@
+import json
+from make_csv import make_csv
+from oddsportal_scraper.oddsportal_scraper import let_the_magic_begin
+from soccerstats_scraper.soccerstats_scraper import scrap
+from difflib import SequenceMatcher
+
+soccerstats_data = scrap()
+oddsportal_data = let_the_magic_begin()
+print(json.dumps(oddsportal_data, indent=4))
+
+def find_the_match1(matchname, oddsportaldatas):
+    highest = 0
+    for odds_match in oddsportaldatas:
+        ratio = SequenceMatcher(None, matchname, odds_match.get("match")).ratio()
+        if (ratio > highest):
+            highest = ratio
+            obj = odds_match
+    if (obj and highest >= 0.58):
+        obj["soccestats_name"] = matchname
+        obj["ratio"] = highest
+        return obj
+    return
+
+for match in soccerstats_data:
+    obj = find_the_match1(match[10] + " - " + match[12], oddsportal_data)
+    if (obj):
+        match.append(obj.get("1"))
+        match.append(obj.get("X"))
+        match.append(obj.get("2"))
+        for ou in obj.get("ou_stats"):
+            if (ou.get("o/u") in ["Over/Under +1.5", "Over/Under +2", "Over/Under +2.5", "Over/Under +3", "Over/Under +3.5", "Over/Under +4", "Over/Under +4.5"]):
+                match.append(ou.get("over"))
+                match.append(ou.get("under"))
+            else:
+                match.append("None")
+                match.append("None")
+
+make_csv(soccerstats_data)
---
+++ make_csv.py
@@ -0,0 +1,9 @@
+import csv
+
+
+def make_csv(data):
+    header = ["Date", "Country", "BTS", "FTS", "CS", "W%", "TG", "PPG", "GP", "Scope", "Pays", "Time", "Pays", "Scope", "GP", "PPG", "TG", "W%", "CS", "FTS", "BTS", "", "1", "X", "2", "Over 1,5", "Under 1,5", "Over 2", "Under 2", "Over 2,5", "Under 2,5", "Over 3", "Under 3", "Over 3,5", "Under 3,5", "Over 4", "Under 4", "Over 4,5", "Under 4,5"]
+    with open('final.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        writer.writerows(data)
