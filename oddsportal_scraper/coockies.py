a78098956f982bd7f0723926746f22ce17a99cb5
MathieuGery <mathieu.gery@epitech.eu>
2022-06-21 15:16:28 +0200

    Update coockies

--- oddsportal_scraper/coockies.py
+++ oddsportal_scraper/coockies.py
@@ -1,78 +1,124 @@
-coockies = [{
+coockies = [
+  {
     "name": "op_testCountryCode",
     "value": "0",
     "domain": "www.oddsportal.com",
     "path": "/",
-    "expires": 1686352487.267133,
-  },
-  {
-    "name": "op_oddsportal",
-    "value": "3tc8pj1tnmasve6osr7u4en4bhcn2m01",
-    "domain": "www.oddsportal.com",
-    "path": "/",
-    "expires": -1,
+    "expires": 1686350591.687263
   },
   {
     "name": "op_cookie-test",
     "value": "ok",
     "domain": "www.oddsportal.com",
     "path": "/",
-    "expires": 1686352508.500273,
+    "expires": 1687353014.342733
   },
   {
     "name": "op_user_cookie",
-    "value": "3404859232",
+    "value": "3404747820",
     "domain": "www.oddsportal.com",
     "path": "/",
-    "expires": 1657408508.403103,
+    "expires": 1658408960.170792
   },
   {
     "name": "op_user_hash",
-    "value": "db829adc70ce0275d868d9f7b0817eac",
+    "value": "8c3513440835f55917625ed918f758c2",
     "domain": "www.oddsportal.com",
     "path": "/",
-    "expires": 1657408508.403126,
+    "expires": 1658408960.170809
   },
   {
     "name": "op_user_time",
-    "value": "1654816488",
+    "value": "1654814593",
     "domain": "www.oddsportal.com",
     "path": "/",
-    "expires": 1657408508.403149,
+    "expires": 1658408960.170827
   },
   {
-    "name": "op_user_time_zone",
-    "value": "2",
+    "name": "op_oddsportal",
+    "value": "n9al5q2eb1fh9s590fv6meu99nslld3g",
     "domain": "www.oddsportal.com",
     "path": "/",
-    "expires": 1657408508.403055,
+    "expires": -1
   },
   {
-    "name": "_sg_b_v",
-    "value": "1%3B518%3B1654816487",
-    "domain": "www.oddsportal.com",
-    "path": "/",
-    "expires": 1662592508,
-  },
-  {
-    "name": "op_user_full_time_zone",
-    "value": "39",
-    "domain": "www.oddsportal.com",
-    "path": "/",
-    "expires": 1657408508.403082,
-  },
-  {
-    "name": "OptanonConsent",
-    "value": "isGpcEnabled=0&datestamp=Fri+Jun+10+2022+01%3A15%3A09+GMT%2B0200+(heure+d%E2%80%99%C3%A9t%C3%A9+d%E2%80%99Europe+centrale)&version=6.30.0&isIABGlobal=false&hosts=&consentId=7011bfb9-5a60-40ec-9943-a0efba7f04be&interactionCount=1&landingPath=https%3A%2F%2Fwww.oddsportal.com%2Fmatches%2Fsoccer%2F20220611%2F&groups=C0001%3A1%2CC0002%3A0%2CC0004%3A0%2CSTACK42%3A0",
+    "name": "OptanonAlertBoxClosed",
+    "value": "2022-06-21T13:09:17.312Z",
     "domain": ".oddsportal.com",
     "path": "/",
-    "expires": 1686352509,
+    "expires": 1687352957,
     "sameSite": "Lax"
   },
   {
-    "name": "_sg_b_p",
-    "value": "%2Fmatches%2Fsoccer%2F20220611%2F%2C%2Fmatches%2Fsoccer%2F20220611%2F%2C%2Fmatches%2Fsoccer%2F20220611%2F",
+    "name": "eupubconsent-v2",
+    "value": "CPa8Z2kPa8Z3lAcABBENCUCsAP_AAAAAAChQIkNf_X__b3_j-_5_f_t0eY1P9_7_v-0zjhfdt-8N2f_X_L8X42M7vF36pq4KuR4Eu3LBIQdlHOHcTUmw6okVrzPsbk2cr7NKJ7PEmnMbO2dYGH9_n93TuZKY7_____7z_v-v_v____f_7-3f3__5_3_--_e_V_99zfn9_____9nP___9v-_9_3gH_AAAAMEgAgLzFQAQF5jIAIC8x0AEBeZKACAvMpABAXmA.f_gAAAAAAAAA",
+    "domain": ".oddsportal.com",
+    "path": "/",
+    "expires": 1687352957,
+    "sameSite": "Lax"
+  },
+  {
+    "name": "_gid",
+    "value": "GA1.2.1987871459.1655816957",
+    "domain": ".oddsportal.com",
+    "path": "/",
+    "expires": 1655903415
+  },
+  {
+    "name": "_gat_UA-821699-19",
+    "value": "1",
+    "domain": ".oddsportal.com",
+    "path": "/",
+    "expires": 1655817017
+  },
+  {
+    "name": "op_user_time_zone",
+    "value": "0",
     "domain": "www.oddsportal.com",
     "path": "/",
-    "expires": 1654817709,
-  }]
+    "expires": 1658408960.170754
+  },
+  {
+    "name": "op_user_full_time_zone",
+    "value": "31",
+    "domain": "www.oddsportal.com",
+    "path": "/",
+    "expires": 1658408960.170774
+  },
+  {
+    "name": "_sg_b_v",
+    "value": "2%3B142%3B1655816951",
+    "domain": "www.oddsportal.com",
+    "path": "/",
+    "expires": 1663593014
+  },
+  {
+    "name": "OptanonConsent",
+    "value": "isGpcEnabled=0&datestamp=Tue+Jun+21+2022+15%3A10%3A15+GMT%2B0200+(heure+d%E2%80%99%C3%A9t%C3%A9+d%E2%80%99Europe+centrale)&version=6.30.0&isIABGlobal=false&hosts=&consentId=1f884dac-f830-4842-913c-5bd1a81c2c84&interactionCount=2&landingPath=NotLandingPage&groups=C0001%3A1%2CC0002%3A1%2CC0004%3A1%2CSTACK42%3A1&AwaitingReconsent=false&geolocation=FR%3BHDF",
+    "domain": ".oddsportal.com",
+    "path": "/",
+    "expires": 1687353015,
+    "sameSite": "Lax"
+  },
+  {
+    "name": "_ga_5YY4JY41P1",
+    "value": "GS1.1.1655816950.1.1.1655817015.0",
+    "domain": ".oddsportal.com",
+    "path": "/",
+    "expires": 1718889015
+  },
+  {
+    "name": "_ga",
+    "value": "GA1.2.2044077275.1655816951",
+    "domain": ".oddsportal.com",
+    "path": "/",
+    "expires": 1718889015
+  },
+  {
+    "name": "_sg_b_p",
+    "value": "%2Fmatches%2Fsoccer%2F20220622%2F%2C%2Fmatches%2Fsoccer%2F20220622%2F%2C%2Fmatches%2Fsoccer%2F20220622%2F%2C%2Fmatches%2Fsoccer%2F20220622%2F",
+    "domain": "www.oddsportal.com",
+    "path": "/",
+    "expires": 1655818215
+  }
+]
