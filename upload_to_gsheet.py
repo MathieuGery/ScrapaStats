c9a379491896c0a73ab7afd99b31b4be7806858c
MathieuGery <mathieu.gery@epitech.eu>
2022-06-25 17:17:54 +0200

    Uplaod it to gsheet

--- upload_to_gsheet.py
+++ upload_to_gsheet.py
@@ -12,9 +12,19 @@ def upload_to_gsheet():
         sheet = client.open('Soccer')
     except:
         sheet = client.create('Soccer')
+    
+    try:
+        sheet2 = client.open('Soccer-Scores')
+    except:
+        sheet2 = client.create('Soccer-Scores')
+    
     for email in emails:
         sheet.share(email, perm_type='user', role='writer')
+        sheet2.share(email, perm_type='user', role='writer')
 
     print(sheet.id)
+    print(sheet2.id)
     content = open('./results/final.csv', 'r').read()
     client.import_csv(sheet.id, content)
+    content = open('./results/yesterday_scores.csv', 'r').read()
+    client.import_csv(sheet2.id, content)
